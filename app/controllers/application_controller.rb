269b8089eb7d3c8443c0cbffa4e256855591d3e6
Yoon Koh <ykoh2@chicagobooth.edu>
2023-04-11 16:07:44 +0000

    square/new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb" })
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 
+  get("/square/new", { :controller => "application", :action => "blank_square_form" })
+
 end
