9096bb4cfcc4b7e746300b24cf872cc9de201d16
Yoon Koh <ykoh2@chicagobooth.edu>
2023-04-11 16:26:40 +0000

    square working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_form.html.erb" })
   end
 
+  def calculate_square
+
+    @num = params.fetch("elephant").to_f
+    @square_of_num = @num ** 2
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   get("/square/new", { :controller => "application", :action => "blank_square_form" })
 
+  get("square/results", { :controller => "application", :action => "calculate_square" })
+
+
 end
