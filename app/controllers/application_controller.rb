32ad58bd834ecec2f9cae66009081c6b9da62d44
Yoon Koh <ykoh2@chicagobooth.edu>
2023-04-11 22:55:24 +0000

    square root done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,18 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_results.html.erb"})
   end
 
+  def blank_square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb" })
+  end
+
+  def calculate_square_root
+
+    @num = params.fetch("number").to_f
+    @square_root_of_num = @num ** 0.5
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
+  end
+
+
 
   def blank_random_form
     render({ :template => "calculation_templates/rand_form.html.erb" })
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   get("/random/results", { :controller => "application", :action => "calculate_random"})
 
-  get("")
+  get("square_root/new", { :controller => "application", :action => "blank_square_root_form" })
+
+  get("square_root/results", { :controller => "application", :action => "calculate_square_root" })
+
+  
 
 end
